20ff5e50accf2fcc9a5d71140e2d2990bb2a3154
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-03-25 21:38:29 +0000

    Add types

--- src/dataset/casia_webface/download.py
+++ src/dataset/casia_webface/download.py
@@ -1,11 +1,11 @@
 import kagglehub
 from time import time
 
-def download_casia_webface():
+def download_casia_webface() -> None:
     try:
-        start_time = time()
-        path = kagglehub.dataset_download("cybersimar08/casia-face-dataset")
-        spent_time = (time() - start_time)
+        start_time: float = time()
+        path: str = kagglehub.dataset_download("cybersimar08/casia-face-dataset")
+        spent_time: float = (time() - start_time)
         print(f"CASIA WebFace dataset downloaded successfully in {spent_time} seconds.\nDataset path: {path}")
     except Exception as e:
         print(f"Unexpected error occurred while downloading CASIA WebFace dataset: {e}")
