011dd06a1c9aa9bd0ecb41c6d09754d84ccae5ea
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-03-28 17:03:23 +0000

    Update maxsim scoring function

--- src/training/scoring.py
+++ src/training/scoring.py
@@ -1,13 +1,19 @@
 from torch._tensor import Tensor
 import torch
-from einops import einsum
+from einops import einsum, rearrange
 
-def maxsim(query_image: Tensor, gallery_image: Tensor) -> Tensor:
+
+def maxsim(query_image: Tensor, gallery_images: Tensor) -> Tensor:
     """
-    Computes a ColBERT-style maximum similarity score between query image and gallery image token representation.
-    Both inputs are expected to be tensors, with dimensions [batch_size, query_len, reduced_dim] and [batch_size, gallery_len, reduced_dim].
+    Computes a single ColBERT-style maximum similarity score between query image and multiple gallery image token representation.
+    Both inputs are expected to be tensors, with dimensions [batch_size, 1, query_len, reduced_dim] and [batch_size, num_images, gallery_len, reduced_dim], respectively. Output is a tensor with dimensions [batch_size, num_images].
     """
-    similarities: Tensor = einsum(query_image, gallery_image, "batch_size query_len reduced_dim, batch_size gallery_len reduced_dim -> batch_size query_len gallery_len")
-    max_similarities, _ = torch.max(similarities, dim=2)
-    scores: Tensor = max_similarities.sum(dim=1)
+    query_image = rearrange(query_image, "b 1 l d -> b l d")
+    similarities: Tensor = einsum(
+        query_image,
+        gallery_images,
+        "batch_size query_len reduced_dim, batch_size num_images gallery_len reduced_dim -> batch_size num_images query_len gallery_len",
+    )
+    max_similarities, _ = torch.max(similarities, dim=-1)
+    scores: Tensor = max_similarities.sum(dim=-1)
     return scores
--- tests/test_similarity_scoring.py
+++ tests/test_similarity_scoring.py
@@ -3,49 +3,77 @@ from pathlib import Path
 import pytest
 import torch
 import sys
+
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.training.scoring import maxsim
 
 
-@pytest.mark.parametrize("batch_size, seq_len, dim", [(2, 4, 8), (1, 3, 16)])
-def test_colbert_batch_size(batch_size, seq_len, dim) -> None:
+@pytest.mark.parametrize(
+    "batch_size, num_images, seq_len, dim", [(1, 1, 8, 16), (1, 3, 16, 8)]
+)
+def test_colbert_batch_size(batch_size, num_images, seq_len, dim) -> None:
     """
     Test colbert_score batch size matching with random inputs of various sizes.
     """
-    query: Tensor = torch.randn(batch_size, seq_len, dim)
-    gallery: Tensor = torch.randn(batch_size, seq_len, dim)
+    query: Tensor = torch.randn(batch_size, 1, seq_len, dim)
+    gallery: Tensor = torch.randn(batch_size, num_images, seq_len, dim)
     scores: Tensor = maxsim(query, gallery)
-    assert scores.shape == (batch_size, ), f"Expected output shape {(batch_size,)} but got {scores.shape}"
+    assert scores.shape == (batch_size, num_images), (
+        f"Expected output shape {(batch_size, num_images)} but got {scores.shape}"
+    )
 
 
 def test_colbert_score_known_equal_dim() -> None:
     """
     Test colbert_score with a small, known example, with the same dimensions between tensors.
     """
-    query = torch.tensor([[[1.0, 0.0], [0.0, 1.0]]])
-    gallery = torch.tensor([[[0.5, 2.0], [2.0, 0.5]]])
-    expected_score = torch.tensor([4.0])
+    query = torch.tensor([[[[0.0, 1.0, 2.0], [0.0, 2.0, 3.0]]]])
+    gallery = torch.tensor(
+        [[[[0.0, 1.0, 3.0], [0.0, 1.0, 2.0]], [[1.0, 2.0, 3.0], [1.0, 3.0, 2.0]]]]
+    )
+    expected_score = torch.tensor([[18.0, 21.0]])
     scores = maxsim(query, gallery)
-    assert torch.allclose(scores, expected_score), f"Expected {expected_score}, but got {scores}"
+    assert torch.allclose(scores, expected_score), (
+        f"Expected {expected_score}, but got {scores}"
+    )
 
 
 def test_colbert_score_known_different_dims() -> None:
     """
     Test colbert_score with a small, known example, with different dimensions between tensors.
     """
-    query = torch.tensor([[[1.0, 0.0], [0.0, 1.0]]])
-    gallery = torch.tensor([[[0.5, 2.0], [2.0, 0.5], [3.0, 0.0]]])
-    expected_score = torch.tensor([5.0])
+    query = torch.tensor([[[[0.0, 1.0, 2.0], [0.0, 2.0, 3.0]]]])
+    gallery = torch.tensor(
+        [
+            [
+                [[0.0, 1.0, 3.0], [0.0, 1.0, 2.0], [0.0, 1.0, 4.0]],
+                [[1.0, 2.0, 3.0], [1.0, 3.0, 2.0], [0.0, 1.0, 5.0]],
+            ]
+        ]
+    )
+    expected_score = torch.tensor([[23.0, 28.0]])
     scores = maxsim(query, gallery)
-    assert torch.allclose(scores, expected_score), f"Expected {expected_score}, but got {scores}"
+    assert torch.allclose(scores, expected_score), (
+        f"Expected {expected_score}, but got {scores}"
+    )
 
 
 def test_colbert_score_mismatch_batch_size() -> None:
     """
     Confirm that colbert_score raises an error when the batch sizes do not match.
     """
-    query = torch.randn(2, 4, 8)
-    gallery = torch.randn(3, 4, 8)
+    query = torch.randn(3, 1, 4, 8)
+    gallery = torch.randn(2, 3, 4, 8)
+    with pytest.raises(RuntimeError):
+        maxsim(query, gallery)
+
+
+def test_colbert_score_too_many_query_images() -> None:
+    """
+    Confirm that colbert_score raises an error when the batch sizes do not match.
+    """
+    query = torch.randn(3, 2, 4, 8)
+    gallery = torch.randn(3, 3, 4, 8)
     with pytest.raises(RuntimeError):
         maxsim(query, gallery)
