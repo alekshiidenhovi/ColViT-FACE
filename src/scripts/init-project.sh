77592b7600de0047c87c6a1ab0592c0eb7d4c362
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-03-29 02:13:26 +0000

    Update script

--- src/scripts/init-project.sh
+++ src/scripts/init-project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/ColViT-FACE/scripts/install-uv.sh
 source ~/ColViT-FACE/scripts/create-venv.sh
-source ~/ColViT-FACE/scripts/sync-deps.sh
+source ~/ColViT-FACE/scripts/sync-deps-dev.sh
