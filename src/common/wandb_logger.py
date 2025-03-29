5aefb535faa57373cbe71be651ac4b52737c6ab1
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-03-29 02:24:09 +0000

    add documentation to wandb logger

--- src/common/wandb_logger.py
+++ src/common/wandb_logger.py
@@ -4,6 +4,23 @@ from dotenv import load_dotenv
 from pytorch_lightning.loggers import WandbLogger
 
 def init_wandb_logger():
+    """
+    Initialize and configure a Weights & Biases logger.
+
+    This function loads environment variables, authenticates with W&B using the API key,
+    and creates a WandbLogger instance for experiment tracking.
+
+    Returns
+    -------
+    WandbLogger
+        Configured W&B logger instance that can be used with PyTorch Lightning.
+
+    Notes
+    -----
+    Requires the following environment variables to be set:
+    - WANDB_API_KEY: The Weights & Biases API key for authentication
+    - WANDB_PROJECT: The name of the W&B project to log to
+    """
     load_dotenv()
     wandb_api_key = os.getenv("WANDB_API_KEY")
     wandb_project = os.getenv("WANDB_PROJECT")
