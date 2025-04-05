d6f38f2292ff40f2d5ed7c7466e2a1b6953c6d58
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-05 22:56:49 +0000

    Fix recursive bug in partition

--- src/benchmark/utils.py
+++ src/benchmark/utils.py
@@ -20,29 +20,33 @@ def partition_lfw_images(dir_path: str, max_images_per_identity: int):
     """Partition LFW images into identities with a single image and those with multiple images. Return also the corresponding image paths.
     """
     
-    all_identities = [
+    identities = [
         d for d in os.listdir(dir_path)
         if os.path.isdir(os.path.join(dir_path, d))
     ]
+    all_identities = []
     all_image_paths = []
     all_image_path_to_identity = {}
     test_identities = []
     test_image_paths = []
     test_image_path_to_identity = {}
     
-    for identity in all_identities:
+    for identity in identities:
         identity_path = os.path.join(dir_path, identity)
-        image_file_names = [img for img in os.listdir(identity_path) 
-                  if img.lower().endswith((".jpg", ".jpeg", ".png"))]
+        image_file_names = [
+            img 
+            for img in os.listdir(identity_path) 
+            if img.lower().endswith((".jpg", ".jpeg", ".png"))
+        ]
         image_file_names = image_file_names[:max_images_per_identity]
         if len(image_file_names) > 1:
-            test_identities.append(identity)
-            test_image_paths.extend([os.path.join(identity_path, img_file_name) for img_file_name in image_file_names])
             for img_file_name in image_file_names:
+                test_identities.append(identity)
                 image_path = os.path.join(identity_path, img_file_name)
+                test_image_paths.append(image_path)
                 test_image_path_to_identity[image_path] = identity
-        all_identities.append(identity)
         for img_file_name in image_file_names:
+            all_identities.append(identity)
             image_path = os.path.join(identity_path, img_file_name)
             all_image_paths.append(image_path)
             all_image_path_to_identity[image_path] = identity
